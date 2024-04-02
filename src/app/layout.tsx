b1e01b0a06d86fb88c6b00ceb35ab5e6714054f0
Farouk Allani <81933547+farouk-allani@users.noreply.github.com>
2024-04-02 14:01:53 +0100

    feat: add dropdown component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import { Providers } from "../redux/provider";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 const pjs = Plus_Jakarta_Sans({ subsets: ["latin"], display: "swap" });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={pjs.className}>
       <Providers>
+        <Navbar />
         {children}
       </Providers>  
         </body>
