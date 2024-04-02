f56c49867b7bc2b7ddc4be876f4d2111596caed5
Farouk Allani <81933547+farouk-allani@users.noreply.github.com>
2024-04-02 11:37:40 +0100

    feat: implement redux provider in app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import { Providers } from "../redux/provider";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const pjs = Plus_Jakarta_Sans({ subsets: ["latin"], display: "swap" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={pjs.className}>
+      <Providers>
+        {children}
+      </Providers>  
+        </body>
     </html>
   );
 }
