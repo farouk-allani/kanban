6220d069e6f25c2b0a65189ef7889098ae2761ea
Farouk Allani <81933547+farouk-allani@users.noreply.github.com>
2024-04-22 15:08:00 +0100

    feat: edit AddAndEditBoardModal

--- src/redux/features/appSlice.ts
+++ src/redux/features/appSlice.ts
@@ -1,17 +1,19 @@
-   import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-   import { RootState } from "../store";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../store";
 
-   // Define the initial state for the slice
-   const initialState = {
-    currentBoardName: "",
-   };
+// Define the initial state for the slice
+const initialState = {
+  currentBoardName: "",
+  // Manage the state for opening and closing the Add and Edit Board modal
+  isAddAndEditBoardModal: { isOpen: false, variant: "" },
+};
 
-   export const features = createSlice({
-   // Name of the slice
-   name: "features",
-   initialState,
-   // Functions that update the initialState are written inside the reducers object
-   reducers: {
+export const features = createSlice({
+  // Name of the slice
+  name: "features",
+  initialState,
+  // Functions that update the initialState are written inside the reducers object
+  reducers: {
     // This function updates the board name when called
     setPageTitle: (state, action: PayloadAction<string>) => {
       state.currentBoardName = action.payload;
@@ -19,15 +21,36 @@
     setCurrentBoardName: (state, action: PayloadAction<string>) => {
       state.currentBoardName = action.payload;
     },
-   },
-   });
+    // Open the Add and Edit Board modal with a specified variant (add or edit)
+    openAddAndEditBoardModal: (state, { payload }) => {
+      state.isAddAndEditBoardModal.isOpen = true;
+      // Set the kind of modal to open (add board or edit board) based on the variant parameter
+      state.isAddAndEditBoardModal.variant = payload;
+    },
+    // Close the Add and Edit Board modal
+    closeAddAndEditBoardModal: (state) => {
+      state.isAddAndEditBoardModal.isOpen = false;
+      state.isAddAndEditBoardModal.variant = "";
+    },
+  },
+});
 
-   // Export the functions defined inside the reducers here
-   export const { setPageTitle,setCurrentBoardName, } = features.actions;
-   export const getCurrentBoardName = (state: RootState) => state.features.currentBoardName;
+// Export the functions defined inside the reducers here
+export const {
+  setPageTitle,
+  setCurrentBoardName,
+  openAddAndEditBoardModal,
+  closeAddAndEditBoardModal,
+} = features.actions;
+export const getCurrentBoardName = (state: RootState) =>
+  state.features.currentBoardName;
 
-   // Selector function to retrieve the current board name from the state
-   export const getPageTitle = (state: RootState) => state.features.currentBoardName;
+// Selector function to retrieve the current board name from the state
+export const getPageTitle = (state: RootState) =>
+  state.features.currentBoardName;
+export const getAddAndEditBoardModalValue = (state: RootState) => state.features.isAddAndEditBoardModal.isOpen;
+// Selector functions to retrieve isOpen value of state from the isAddAndRditBoardModal state
+export const getAddAndEditBoardModalVariantValue = (state: RootState) => state.features.isAddAndEditBoardModal.variant;
 
-   // Export the reducer for use in the Redux store
-   export default features.reducer;
+// Export the reducer for use in the Redux store
+export default features.reducer;
